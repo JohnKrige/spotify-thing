29527d4ee456aa9eb3e68ab34667f020ce5bc398
JohnKrige <krigejohn@gmail.com>
2021-02-12 09:44:31 +0200

    .

--- routes/spotify.js
+++ routes/spotify.js
@@ -19,7 +19,7 @@ const router = express.Router();
 
 const client_id = '774f74e3029946fe9f5c9ee7a1ee5f3d'; // Your client id
 const client_secret = '352f29ce4cd6414cada4e6afd267675b'; // Your secret
-const redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
+const redirect_uri = 'https://snakeballs.herokuapp.com/'; // Your redirect uri
 
 router.get('/', topListens(5, 'short'), (req, res) => {
   // Check if the spotify token has to be refreshed. Middleware this? 
